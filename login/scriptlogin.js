d504826f57289d10da2fdce3ee94816f2b1c1c00
BryanChrisRH <sneakyrdz501.441@gmail.com>
2023-11-08 18:14:14 -0600

    Nuevo Comitt

--- login/scriptlogin.js
+++ login/scriptlogin.js
@@ -1,3 +1,12 @@
+let btnM = document.getElementById('btnM');
+let navLinks = document.querySelector('.nav-links');
+
+btnM.onclick = () =>{
+    btnM.classList.toggle('fa-times')
+    navLinks.classList.toggle('active')
+}
+
+
 $(".input_text").focus(function(){
     $(this).prev('.fa').addclass('glowIcon')
 })
